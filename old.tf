f9167ba2811156fa71f8bae03ec8a0e60a96caaf
unknown <henriquemarrocos@hotmail.com>
2022-08-18 01:09:10 -0400

    Practice project completed

---
+++ old.tf
@@ -0,0 +1,28 @@
+/* provider "aws" {
+  region = "us-east-1"
+  access_key = ""
+  secret_key = ""
+} */
+
+/* resource "aws_vpc" "first-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    "Name" = "production"
+  }
+} */
+
+/* resource "aws_subnet" "subnet-1" {
+  vpc_id = aws_vpc.first-vpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    "Name" = "prod-subnet"
+  }
+} */
+
+/* resource "aws_instance" "my-first-server" {
+  ami = "ami-0729e439b6769d6ab"
+  instance_type = "t2.micro"
+  tags = {
+    "Name" = "1st-terraform-srv"
+  }
+} */
